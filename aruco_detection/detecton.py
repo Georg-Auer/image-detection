2939941e11c49e3425951fa72f9587653fdb5035
G-code-SPOC <georg.auer@gmail.com>
2021-06-10 10:52:35 +0200

    bugfix

--- aruco_detection/detecton.py
+++ aruco_detection/detecton.py
@@ -4,7 +4,28 @@
 import numpy as np
 import cv2
 import cv2.aruco as aruco
+
 cap = cv2.VideoCapture(0)
+
+
+aruco_dict = aruco.custom_dictionary(0, 4, 1)
+#add empty bytesList array to fill with 4 markers later, (first 4 next line)
+aruco_dict.bytesList = np.empty(shape = (4, 2, 4), dtype = np.uint8)
+
+#add new marker(s)
+#each 1 per row represents a white pixel
+#new symbols and letters can be painted this way
+mybits = np.array([[1,1,1,1],[1,1,0,0],[0,0,1,1],[1,1,1,1]], dtype = np.uint8)
+aruco_dict.bytesList[0] = aruco.Dictionary_getByteListFromBits(mybits)
+mybits = np.array([[0,1,1,0],[1,0,0,1],[1,1,1,0],[1,0,0,0],], dtype = np.uint8)
+aruco_dict.bytesList[1] = aruco.Dictionary_getByteListFromBits(mybits)
+mybits = np.array([[0,1,1,0],[1,0,0,1],[1,0,0,1],[0,1,1,0]], dtype = np.uint8)
+aruco_dict.bytesList[2] = aruco.Dictionary_getByteListFromBits(mybits)
+mybits = np.array([[0,1,1,1],[1,0,0,0],[1,0,0,0],[0,1,1,1]], dtype = np.uint8)
+aruco_dict.bytesList[3] = aruco.Dictionary_getByteListFromBits(mybits)
+#This code can be used with python3 and cv2
+#to identify custom Aruco codes in connection with part 1
+#It will not run in colab but is shorter and easy to understand
 
 while(True):
     # Capture frame-by-frame
